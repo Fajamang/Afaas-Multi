ba8357d6f86d8c5e179426d5817b3985a255ed57
Fajamang <mike6514@yahoo.com>
2025-06-24 18:51:18 +0200

    Delete utils/uploadHandler.ts

--- utils/uploadHandler.ts
+++
@@ -1,9 +0,0 @@
-import { chunkText } from "./chunkText";
-import { embedChunks } from "./embedChunks";
-import { storeChunks } from "./storeChunks";
-
-export async function handleUpload(tenant: string, rawText: string): Promise<void> {
-  const chunks = chunkText(rawText);
-  const embeddings = await embedChunks(chunks);
-  await storeChunks(tenant, chunks, embeddings);
-}
